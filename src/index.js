951f59239fcc8e47f0985d326c5d8f31204cb10d
Mac Rowe <m.rowe42@gmail.com>
2021-06-01 16:06:25 -0400

    commit tree 94acece1e30f8e36e0fe7704700dbefb653a9535

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function fetchImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+}
+
+function renderImages()
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages();
+  });
